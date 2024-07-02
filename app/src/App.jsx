596a7892d13ab6caa7455d10ed939d08f1176014
planetofthecode <rajkumar.karnakar@outlook.com>
2024-07-02 16:11:38 +0530

    local and server data functions are added in container component design pattern

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -9,18 +9,29 @@ const getServerData = url => async ()=>{
   const resp = await axios.get(url);
 return resp.data};
 
+const getLocalData = key =>()=>{
+  return localStorage.getItem(key);
+}
+
+const Text = ({message}) =>
+  <h2>{message}</h2>
+
+
 function App() {
   return (
   <>
-  <ResourceLoader resourceURL={"/users/101"} resourceName={"user"}>
-      <UserInfo/>
-    </ResourceLoader>
-    <ResourceLoader resourceURL={"/products/1002"} resourceName={"product"}>
-      <ProductInfo/>
-    </ResourceLoader>
-    <DataSource getDataFunc={getServerData("/users/102")} resourceName="user" >
-      <UserInfo/>
-    </DataSource>
+    <ResourceLoader resourceURL={"/users/101"} resourceName={"user"}>
+    <UserInfo/>
+  </ResourceLoader>
+  <ResourceLoader resourceURL={"/products/1002"} resourceName={"product"}>
+    <ProductInfo/>
+  </ResourceLoader>
+  <DataSource getDataFunc={getServerData("/users/102")} resourceName="user" >
+    <UserInfo/>
+  </DataSource>
+ <DataSource getDataFunc={getLocalData('message')} resourceName="message">
+    <Text />
+  </DataSource>
   </>
     
   );
