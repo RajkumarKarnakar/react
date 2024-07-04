0bbce1b127f1a2dcaf7319713aa18c76c3eeb100
planetofthecode <rajkumar.karnakar@outlook.com>
2024-07-04 15:15:17 +0530

    controlled onboarding flow complete

--- app/src/App.js
+++ app/src/App.js
@@ -1,31 +1,53 @@
-import { UncontrolledOnboardingFlow } from './UncontrolledOnboarding';
+import { useState } from 'react';
+import { ControlledOnboardingFlow } from './ControlledOnboardingFlow';
 
 const StepOne = ({ goToNext }) => (
 	<>
 	<h1>Step 1</h1>
-	<button onClick={goToNext}>Next</button>
+	<button onClick={()=>goToNext({name:'rajkumar'})}>Next</button>
 	</>
 );
 const StepTwo = ({ goToNext }) => (
 	<>
 	<h1>Step 2</h1>
-	<button onClick={goToNext}>Next</button>
+	<button onClick={()=>goToNext({age: 60})}>Next</button>
 	</>
 );
 const StepThree = ({ goToNext }) => (
 	<>
 	<h1>Step 3</h1>
-	<button onClick={goToNext}>Next</button>
+  <p>Congratulations you can avail the senior citizen discount</p>
+	<button onClick={()=>goToNext({hairColor:'black'})}>Next</button>
+	</>
+);
+const StepFour = ({ goToNext }) => (
+	<>
+	<h1>Step 4</h1>
+	<button onClick={()=>goToNext({hairColor:'black'})}>Next</button>
 	</>
 );
 
 function App() {
+  const [onboardingData, setOnboardingData] = useState({});
+	const [currentIndex, setCurrentIndex] = useState(0);
+
+  const onNext = stepData => {
+	    setOnboardingData({
+        ...onboardingData,
+        ...stepData,
+    });
+     setCurrentIndex(currentIndex+1);
+  }
+
 	return (
-		<UncontrolledOnboardingFlow>
+		<ControlledOnboardingFlow
+        currentIndex={currentIndex}
+        onNext={onNext}>
 			<StepOne />
 			<StepTwo />
-			<StepThree />
-		</UncontrolledOnboardingFlow>
+			{onboardingData.age >= 60 &&<StepThree />}
+      <StepFour/>
+		</ControlledOnboardingFlow>
 	);
 }
 
