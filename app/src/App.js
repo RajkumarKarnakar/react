98e5872ffe03f5387e089fa4e665b16188ebf857
planetofthecode <rajkumar.karnakar@outlook.com>
2024-07-06 23:16:09 +0530

    use of custom hooks as container comonents

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,13 @@
+import { ProductInfo } from "./ProductInfo";
 import { UserInfo } from "./UserInfo";
 
 function App() {
 	return (
   <>
     <UserInfo userId={102}/>
+    <ProductInfo productId={1001}/>
     <UserInfo userId={101}/>
+    <ProductInfo productId={1002}/>
   </>
   );
 }
--- app/src/UserInfo.js
+++ app/src/UserInfo.js
@@ -1,18 +1,36 @@
-import { useUser } from "./useUser.js";
+import { useDataSource } from "./useDataSource.js";
+import axios from "axios";
+const serverResource = resourceUrl => async () => {
+    const response = await axios.get(resourceUrl);
+    return response.data;
+};
 
-export const UserInfo = ({userId}) =>{
-    const user = useUser(userId);
-    const {name, age, hairColor, hobbies} = user || {};
+// const localStorageResource = key => () => {
+//     // Wrap the value in a promise
+//     return Promise.resolve(localStorage.getItem(key));
+// };
+
+export const UserInfo = ({ userId }) => {
+    const user = useDataSource(serverResource(`/users/${userId}`));
+    //const message = useDataSource(localStorageResource('message'));
+
+    console.log("Hi"); // This will log only once
+
+    const { name, age, hairColor, hobbies } = user || {};
 
     return user ? (
-    <>
-    <h2>{name}</h2>
-    <p>Age: {age} years</p>
-    <p>Hair Color: {hairColor}</p>
-    <h3>Hobbies:</h3>
-    <ul>
-        {hobbies.map(hobby=><li key={hobby}>{hobby}</li>)}
-    </ul>
-    </>
-    ): <p>Loading...</p>
-}
+        <>
+            <h2>{name}</h2>
+            <p>Age: {age} years</p>
+            <p>Hair Color: {hairColor}</p>
+            <h3>Hobbies:</h3>
+            <ul>
+                {hobbies.map(hobby => (
+                    <li key={hobby}>{hobby}</li>
+                ))}
+            </ul>
+        </>
+    ) : (
+        <p>Loading...</p>
+    );
+};
