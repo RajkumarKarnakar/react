68912376fcf308c8df77de283b6480825f8cdf2b
planetofthecode <rajkumar.karnakar@outlook.com>
2024-07-07 10:50:27 +0530

    recursive component

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,28 @@
-import { ProductInfo } from "./ProductInfo";
-import { UserInfo } from "./UserInfo";
+import { RecursiveComonent } from "./RecursiveComponent";
 
 function App() {
+const nestedObject = {
+  a:1,
+  b:{
+    b23 : "Hello",
+  },
+  b3:{
+    b31:{
+      message: "Hi,"
+    },
+    b32:{
+      message : 'Hi',
+    }  
+  },
+  c: {
+    c1: 2,
+    c2: 3,
+  }
+}
+
 	return (
   <>
-    <UserInfo userId={102}/>
-    <ProductInfo productId={1001}/>
-    <UserInfo userId={101}/>
-    <ProductInfo productId={1002}/>
+<RecursiveComonent data={nestedObject}/>
   </>
   );
 }
